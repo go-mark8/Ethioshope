a4b560c79fd2792e033a4e354c50372512003643
Michael <michael_we@icloud.com>
2025-12-10 09:50:10 -0800

    Update auth_screen.dart

--- lib/src/ui/auth/auth_screen.dart
+++ lib/src/ui/auth/auth_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import '../../theme/app_theme.dart';
 
 class AuthScreen extends StatefulWidget {
